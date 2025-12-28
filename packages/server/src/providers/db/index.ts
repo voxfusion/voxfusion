f38aa19f47fed19a43dfa1016e30816ce889a426
gregortokarev <e@tokarev.work>
2025-12-28 11:00:11 +0300

    feat: add google oauth

--- packages/server/src/providers/db/index.ts
+++ packages/server/src/providers/db/index.ts
@@ -2,7 +2,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-console.log("DATABASE_URL", process.env.DATABASE_URL);
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
