68c070ea1d1b56fcad73daf514979c9c500bf259
gregortokarev <e@tokarev.work>
2025-12-28 10:45:44 +0300

    fix: better auth

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -4,6 +4,9 @@ import { db } from "./providers/db";
 import * as schema from "./providers/db/schema";
 
 export const auth = betterAuth({
+	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+	basePath: "/api/auth",
+	secret: process.env.BETTER_AUTH_SECRET!,
 	database: drizzleAdapter(db, {
 		provider: "pg",
 		schema
