8b30cb4c70b4cc96ba8c44c404bc56d37640f731
gregortokarev <e@tokarev.work>
2025-12-28 11:00:11 +0300

    feat: add google oauth

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -3,13 +3,20 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./providers/db";
 import * as schema from "./providers/db/schema";
 
+const betterAuthSchema = {
+	user: schema.users,
+	session: schema.sessions,
+	account: schema.accounts,
+	verification: schema.verifications,
+};
+
 export const auth = betterAuth({
 	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
 	basePath: "/api/auth",
 	secret: process.env.BETTER_AUTH_SECRET!,
 	database: drizzleAdapter(db, {
 		provider: "pg",
-		schema
+		schema: betterAuthSchema,
 	}),
 	socialProviders: {
 		google: {
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,9 +4,13 @@ import { authRoutes } from "./routes/auth";
 import { Groq } from "groq-sdk";
 import { transcribeRoutes } from "./routes/transcribe";
 import { BunAdapter } from "elysia/adapter/bun";
+import { createAuthClient } from "better-auth/client";
 
 const app = new Elysia({ prefix: "/api", adapter: BunAdapter })
-	.use(cors())
+	.use(cors({
+		origin: true,
+		credentials: true,
+	}))
 	.decorate("groq", new Groq({ apiKey: process.env.GROQ_API_KEY! }))
 	.use(transcribeRoutes)
 	.get("/", () => ({
@@ -14,6 +18,13 @@ const app = new Elysia({ prefix: "/api", adapter: BunAdapter })
 		version: "0.1.0",
 		status: "healthy",
 	}))
+	.get("/google-auth-callback", async () => {
+		const authClient = createAuthClient()
+		const data = await authClient.signIn.social({
+			provider: "google",
+		})
+		return data
+	})
 	.get("/health", () => ({ status: "ok" }))
 	.use(authRoutes)
 	.listen(3000);
