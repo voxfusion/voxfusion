c5036bd35c89b65205bb7a4e305d4fd28a904bd7
gregortokarev <e@tokarev.work>
2026-02-11 18:11:12 +0300

    fix: postgres issues

--- packages/server/src/providers/db/index.ts
+++ packages/server/src/providers/db/index.ts
@@ -8,5 +8,10 @@ if (!connectionString) {
 	throw new Error("DATABASE_URL environment variable is not set");
 }
 
-const client = postgres(connectionString);
+const client = postgres(connectionString, {
+	max: 10,
+	idle_timeout: 20,
+	connect_timeout: 30,
+	max_lifetime: 60 * 10,
+});
 export const db = drizzle(client, { schema });
